302e56acc182920afa2b1175a184898029c4f2a7
leeyanlau <leeyanlau@hotmail.com>
2023-05-10 01:52:13 +0800

    feat: add ability to get necessary data from repo data object

--- src/components/repoCard.jsx
+++ src/components/repoCard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getRepos } from "./../redux/ducks/repos";
+import { getRepos } from "../redux/ducks/repos";
 
 const RepoCard = ({ repo }) => {
   const dispatch = useDispatch();
@@ -12,18 +12,18 @@ const RepoCard = ({ repo }) => {
   const repos = useSelector((state) => state.repos.repos);
 
   console.log({ repos });
-  // const { name, description, stargazers_count, forks_count, watchers, language } = repos[0];
+  const { name, description, stargazers_count, forks_count, watchers, language } = repos[2];
   return (
     <div className="card">
       CARD
-      {/* <div className="card_title">Title: {name}</div>
+      <div className="card_title">Title: {name}</div>
       <div className="card_desc">Description: {description}</div>
       <div className="card_details">
         <div className="card_details_stars">Stars: {stargazers_count}</div>
         <div className="card_details_forks">Forks: {forks_count}</div>
         <div className="card_details_watchers">Watchers: {watchers}</div>
         <div className="card_details_language">Language: {language}</div>
-      </div> */}
+      </div>
     </div>
   );
 };
