203fabb8e4ae04bbdfa99ec50c25f4d3bfef52c5
leeyanlau <leeyanlau@hotmail.com>
2023-05-10 22:32:21 +0800

    feat: map repos into each card and display it onto homepage

--- src/components/repoCard.jsx
+++ src/components/repoCard.jsx
@@ -15,10 +15,10 @@ const RepoCard = () => {
   console.log(repos);
 
   return (
-    <div className="card">
+    <div className="card_container">
       {repos.map((val, key) => {
         return (
-          <>
+          <div className="card" key={key}>
             <div className="card_title">Title: {val.name}</div>
             <div className="card_desc">Description: {val.description}</div>
             <div className="card_details">
@@ -27,7 +27,7 @@ const RepoCard = () => {
               <div className="card_details_watchers">Watchers: {val.watchers}</div>
               <div className="card_details_language">Language: {val.language}</div>
             </div>
-          </>
+          </div>
         );
       })}
     </div>
