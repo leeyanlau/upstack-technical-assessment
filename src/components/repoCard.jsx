59946fbaf6bbca84f81d614cfe47eedbc9caabca
leeyanlau <leeyanlau@hotmail.com>
2023-05-11 23:54:04 +0800

    feat: add infinite scrolling

--- src/components/repoCard.jsx
+++ src/components/repoCard.jsx
@@ -8,11 +8,25 @@ const RepoCard = () => {
   const { repos, searchTerm } = useSelector((state) => state.reposReducer);
   const dispatch = useDispatch();
 
+  //get initial repo data
   useEffect(() => {
     dispatch(getReposFetch());
   }, [dispatch]);
 
-  const reposData = repos.filter((repo) => repo.name.toLowerCase().includes(searchTerm));
+  //check on state change if scroll is used and remove after check
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
+  //if at bottom, get more repo data
+  const handleScroll = () => {
+    if (window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight) {
+      dispatch(getReposFetch());
+    }
+  };
+
+  const reposData = repos.filter((repo) => repo.name.toLowerCase().includes(searchTerm)); //filter search data
 
   return (
     <div className="card_container">
