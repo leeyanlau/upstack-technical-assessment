8c335bd39f519fb26c635ac5bb15babdc65583d0
leeyanlau <leeyanlau@hotmail.com>
2023-05-12 15:43:16 +0800

    feat: try to improve the search term, however unsuccessful

--- src/redux/saga/rootSaga.js
+++ src/redux/saga/rootSaga.js
@@ -2,19 +2,21 @@ import axios from "axios";
 import { call, put, takeLatest, select } from "redux-saga/effects";
 import { getReposFailure, getReposSuccess } from "../state/reposState";
 
-const getData = async (page) => {
-  const url = "https://api.github.com/orgs/reactjs/repos?page=" + page + "&per_page=10&sort=updated_at";
+const getData = async (page, searchTerm) => {
+  const newUrl = "https://api.github.com/search/repositories?q=org:reactjs+" + searchTerm + "&page=" + page + "&per_page=10";
+  // const url = "https://api.github.com/orgs/reactjs/repos?page=" + page + "&per_page=10&sort=updated_at";
+  console.log(newUrl);
   const res = await axios
-    .get(url, { headers: { Authorization: `Bearer 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` } })
-    .then((response) => response);
+    .get(newUrl, { headers: { Authorization: `Bearer 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` } })
+    .then((response) => response.data.items);
   return res;
 };
 
 function* workGetReposFetch() {
   try {
-    const page = yield select((state) => state.reposReducer.page);
-    const response = yield call(getData, page);
-    yield put(getReposSuccess(response.data));
+    const { page, searchTerm } = yield select((state) => state.reposReducer);
+    const response = yield call(getData, page, searchTerm);
+    yield put(getReposSuccess(response));
   } catch (error) {
     yield put(getReposFailure(error));
   }
