da2163d0b061adc00ca37d3571142093ef776b52
leeyanlau <leeyanlau@hotmail.com>
2023-05-12 18:34:44 +0800

    fix: fix api calling & include env file

--- src/redux/saga/rootSaga.js
+++ src/redux/saga/rootSaga.js
@@ -5,9 +5,8 @@ import { getReposFailure, getReposSuccess } from "../state/reposState";
 const getData = async (page, searchTerm) => {
   const url = "https://api.github.com/search/repositories?q=org:reactjs+" + searchTerm + "&page=" + page + "&per_page=10";
   // const url = "https://api.github.com/orgs/reactjs/repos?page=" + page + "&per_page=10&sort=updated_at";
-  console.log(url);
   const res = await axios
-    .get(url, { headers: { Authorization: `Bearer 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` } })
+    .get(url, { headers: { Authorization: `Bearer ${process.env.REACT_APP_SECRET2}` } })
     .then((response) => response.data.items);
   return res;
 };
